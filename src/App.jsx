77b7258a3474e91821d7caee2b139fef8208ea23
vetrivikasS <svetrivikas@gmail.com>
2025-04-28 09:03:59 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react'
 import Navbar from './components/navbar/index'
 import Hero from './components/Hero/index'
 
-import Title from './components/Title/Title'
+import Title from './components/Title/index'
 import About from './components/About/index'
 import Campus from './components/Campus/index'
 import Testimonials from './components/Testimonials/index'
@@ -11,7 +11,7 @@ import Contact from './components/contact/index'
 
 import Programs from './components/programs/index'
 import Footer from './components/footer/index'
-import Videoplayer from './components/Playbutton/videoplayer'
+import Videoplayer from './components/videoplayer/index'
 
 
 
